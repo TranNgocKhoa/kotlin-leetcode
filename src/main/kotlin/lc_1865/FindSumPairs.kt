c252d08963033b0b8f66a42274d80dd3effabcbd
Tran Ngoc Khoa <tranngoc.khoa.spk@gmail.com>
2025-07-06 17:31:35 +0700

    LC-1865

--- src/main/kotlin/lc_1865/FindSumPairs.kt
+++ src/main/kotlin/lc_1865/FindSumPairs.kt
@@ -37,3 +37,55 @@ class FindSumPairs(val nums1: IntArray, val nums2: IntArray) {
         return count
     }
 }
+
+fun main() {
+    val findSumPairs = FindSumPairs(
+        intArrayOf(1, 1, 2, 2, 2, 3),
+        intArrayOf(1, 4, 5, 2, 5, 4)
+    )
+    println("Initial nums2: ${findSumPairs.nums2.contentToString()}")
+    println("--------------------")
+
+    // First count call
+    val count1 = findSumPairs.count(7)
+    println("count(7): $count1") // Expected: 8
+    println("--------------------")
+
+    // First add call
+    findSumPairs.add(3, 2)
+    println("add(3, 2)")
+    println("nums2 after add: ${findSumPairs.nums2.contentToString()}")
+    println("--------------------")
+
+
+    // Second count call
+    val count2 = findSumPairs.count(8)
+    println("count(8): $count2") // Expected: 2
+    println("--------------------")
+
+    // Third count call
+    val count3 = findSumPairs.count(4)
+    println("count(4): $count3") // Expected: 1
+    println("--------------------")
+
+    // Second and third add calls
+    findSumPairs.add(0, 1)
+    println("add(0, 1)")
+    findSumPairs.add(1, 1)
+    println("add(1, 1)")
+    println("nums2 after adds: ${findSumPairs.nums2.contentToString()}")
+    println("--------------------")
+
+    // Final count call
+    val count4 = findSumPairs.count(7)
+    println("count(7): $count4") // Expected: 11
+    println("--------------------")
+
+    // --- Verification of Output ---
+    println("\n--- Summary of Outputs ---")
+    // This corresponds to the list of outputs in the problem description [null, 8, null, 2, 1, null, null, 11]
+    println("Output for count(7): $count1")
+    println("Output for count(8): $count2")
+    println("Output for count(4): $count3")
+    println("Output for count(7): $count4")
+}
